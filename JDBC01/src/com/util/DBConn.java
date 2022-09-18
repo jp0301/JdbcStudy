455a3a9ca120f30de48e9a6a547154efaca5ed56
jp0301 <jp0301@naver.com>
2022-09-18 22:33:12 +0900

    2022-09-18 jdbc

--- JDBC01/src/com/util/DBConn.java
+++ JDBC01/src/com/util/DBConn.java
@@ -1,7 +1,3 @@
-/*===================
-  DBConn.java
-================== */
-
 package com.util;
 
 import java.sql.Connection;
@@ -9,65 +5,71 @@ import java.sql.DriverManager;
 
 public class DBConn
 {
-	private static Connection dbConn;
-	
-	public static Connection getConnection()
-	{
-		if (dbConn == null)
-		{
-			try
-			{
-				String url = "jdbc:oracle:thin:@211.238.142.58:1521:xe";
-				String user = "scott";
-						String pwd = "tiger";
-						
-						Class.forName("orcle.jdbc.driver.OracleDriver");
-						dbConn = DriverManager.getConnection(url, user, pwd);
-				
-			} catch (Exception e) {
-				System.out.println(e.toString());
-			}
-		}
-		
-		return dbConn;
-	}
-	
-	public static Connection getConnection(String url, String user, String pwd)
-	{
-		if (dbConn == null)
-		{
-			try
-			{
-				Class.forName("oracle.jdbc.driver.OracleDriver");
-				dbConn = DriverManager.getConnection(url, user, pwd);
-			} catch (Exception e) {
-				System.out.println(e.toString());
-			}
-		}
-		
-		return dbConn;
-	}
-	
-	
-	public static void close()
-	{
-		if (dbConn != null)
-		{
-			try
-			{
-				if(!dbConn.isClosed())
-				{
-					dbConn.close();
-				}
-			}
-			catch (Exception e) {
-				System.out.println(e.toString());
-			}
-		}
-		
-		dbConn = null;
-	}
-	
-	
-	
+ private static Connection dbConn;
+ 
+ public static Connection getConnection()
+ {
+    if (dbConn == null)
+    {
+       try
+       {
+          //String url = "jdbc:oracle:thin:@211.238.142.58:1521:xe";
+    	   // 학원컴에 문제가 있는건지 일단은 집컴으로 연결해서 scott 계정만들고
+    	   // 일단 연결시켜봄. 결과는 성공.
+          String url = "jdbc:oracle:thin:@localhost:1521:xe";
+          String user = "scott";
+                String pwd = "tiger";
+                
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+                dbConn = DriverManager.getConnection(url, user, pwd);
+          
+       } catch (Exception e) {
+          System.out.println(e.toString());
+       }
+    }
+    
+    return dbConn;
+ }
+ 
+ public static Connection getConnection(String url, String user, String pwd)
+ {
+    if (dbConn == null)
+    {
+       try
+       {
+          Class.forName("oracle.jdbc.driver.OracleDriver");
+          dbConn = DriverManager.getConnection(url, user, pwd);
+       } catch (Exception e) {
+          System.out.println(e.toString());
+       }
+    }
+    
+    return dbConn;
+ }
+ 
+ 
+ public static void close()
+ {
+    if (dbConn != null)
+    {
+       try
+       {
+          if(!dbConn.isClosed())
+          {
+             dbConn.close();
+          }
+       }
+       catch (Exception e) {
+          System.out.println(e.toString());
+       }
+    }
+    
+    dbConn = null;
+ }
+ 
+ 
+ 
 }
+
+
+
